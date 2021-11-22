c77df11707b976e520813ce7f84f8ad1293fb56a
Eduardo Rojas Alvarez <eduardorojasalvarez@comamosya.com>
2021-11-21 18:39:58 -0600

    fix: renamed git app, avoid module name colision

--- git_api/apps.py
+++ git_api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GitConfig(AppConfig):
+class GitApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'git'
+    name = 'git_api'
